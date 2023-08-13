958d8ee441825c3e49a30d821a5ad455c9a6cf08
Sari Saldi <110008177+Sary332@users.noreply.github.com>
2023-08-13 08:41:35 +0700

    Update Product_Review_Analysis.sql

--- Product_Review_Analysis/Product_Review_Analysis.sql
+++ Product_Review_Analysis/Product_Review_Analysis.sql
@@ -1,51 +1,11 @@
 -----------------------Customer Satisfaction and Product Reviews Analysis------------------------
 
-'''
-Purpose: The purpose of this project is to analyze customer satisfaction and product reviews to gain insights 
-		 into the factors affecting customer sentiment and identify potential areas for improvement.
-
-Task: Analyze the customer reviews and associated product attributes to understand customer satisfaction 
-	  levels and product performance.
-	  
-Goal: The goal of this project is to provide actionable insights to the e-commerce company (Olist, in this case) 
-	  to enhance customer satisfaction, identify popular product categories, and improve product offerings based
-	  on customer feedback.
-	  
-Analysis Tasks:
-
-Customer Review Sentiment: Analyze the distribution of review scores to understand the overall customer satisfaction 
-						   level. Calculate the average review score to get an overall sentiment rating.
-
-Product Category Sentiment: Determine which product categories receive the highest and lowest average review 
-							scores. Identify the best and worst-performing product categories in terms of 
-							customer satisfaction.
-
-Top Positive and Negative Reviews: Identify the top positive and negative reviews based on review scores and 
-								   comments. Analyze common themes or issues mentioned in negative reviews.
-
-Geographical Analysis: Analyze customer satisfaction levels based on their location (e.g., city, state) 
-					   to identify regional variations in sentiment.
-
-Product Attributes and Reviews: Analyze the relationship between product attributes and review scores. 
-								Determine if specific product attributes correlate with higher customer 
-								satisfaction.
-
-Time-based Analysis: Examine customer satisfaction trends over time to identify any significant changes 
-					 in sentiment.
-
-Recommendations for Improvement: Based on the analysis, provide recommendations to improve customer satisfaction
-								 and address any common issues mentioned in negative reviews.
-
-'''
 
 DROP TABLE IF EXISTS geolocation CASCADE;
 DROP TABLE IF EXISTS customers CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS order_payments;
 DROP TABLE IF EXISTS order_review;
 DROP TABLE IF EXISTS products CASCADE;
-DROP TABLE IF EXISTS seller CASCADE;
-DROP TABLE IF EXISTS order_items;
 
 ----------------------------------------------------------------------------------------------------------------
 
@@ -96,27 +56,6 @@ CREATE TABLE products
 	product_height_cm		INT,
 	product_width_cm		INT
 );
-
-
-CREATE TABLE seller
-(
-	seller_id				VARCHAR(100) PRIMARY KEY,
-	zip_code				INT,
-	seller_city				VARCHAR(50),
-	seller_state			VARCHAR(10)
-);
-
-CREATE TABLE order_items
-(
-	order_id				VARCHAR(100) REFERENCES orders(order_id),
-	order_item_id			INT,
-	product_id				VARCHAR(100) REFERENCES products(product_id),
-	seller_id				VARCHAR(100) REFERENCES seller(seller_id),
-	shipping_limit_date		TIMESTAMP,
-	price					DECIMAL(10,2),
-	freight_value			DECIMAL(10,2)
-);
-
 ------------------------------------------------BATAS--------------------------------------------------
 
 --Customer Review Sentiment:
