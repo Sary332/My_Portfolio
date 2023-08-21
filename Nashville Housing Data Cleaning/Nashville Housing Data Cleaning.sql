58d52bf604f74e9d212cd909bb3eb2ea907a3cdd
Sari Saldi <110008177+Sary332@users.noreply.github.com>
2023-08-21 20:45:16 +0700

    Update Nashville Housing Data Cleaning.sql

--- Nashville Housing Data Cleaning/Nashville Housing Data Cleaning.sql
+++ Nashville Housing Data Cleaning/Nashville Housing Data Cleaning.sql
@@ -1,3 +1,4 @@
+--------------------------------------------------------------- Nashville Housing Data Cleaning -------------------------------------------------------------------------
 CREATE TABLE Housing 
 			(
 			    UniqueID 			INT Primary Key,
@@ -22,50 +23,43 @@ CREATE TABLE Housing
 			);
 ------------------------------------------------------------------------------------
 /*
-
-Cleaning Data in SQL Queries
+ 
+Cleaning Data Using PostgreSQL
 
 */
 
+---- Cheking the table
 select *
 from housing
 limit 100
 
--- Standardize Date Format 
+---- Standardize Date Format 
 
-select saledate
-from housing
+select saledate  
+from   housing
 
 alter table housing
-alter column saledate type date using saledate:: date;
+alter column saledate type date using saledate :: date;
 
 -- Populate propertyaddress data
+/*
+so first I'm gonna check does the column propertyaddress contain any null values. And, after checking the null values in propertyaddress, I found out that there is many null values in it, and we dont want that happend, so we have to find a way to solved. We can see that if the "parcelid" has the same value, then the "propertyaddress" value will also be the same. So something we can do is : If there is a null value in the column propertyaddress, then fill that null value with the value in the propertyaddress column itself that has the same value as the parcelid.
 
-so first we're gonna check does the column propertyaddress contain any null values.
-And, after checking the null values in propertyaddress we found out that there is many null values
-in it, and we dont want that happend, so we have to find a way to solved. We can see that if the parcelid
-has the same value, then the propertyaddress value will also be the same.so something we can do is : 
-If there is a null value in the column propertyaddress, then fill that null value with the value in the propertyaddress
-column itself that has the same value as the parcelid."
+I'm gonna use self join
+*/
 
-we are gonna use self join
-'''
-select *
-from housing
-order by parcelid 
+-- a) Test the query using coalesce and compare the original values that are still null with the values that have been filled using the coalesce function.
 
-
-#dibaca : jika colom propertyaddres ditabel a null, maka isilah dia dengan propertyaddress yang ada di tabel b.
-
-select a.parcelid,a.propertyaddress, b.parcelid,b.propertyaddress,COALESCE(a.propertyaddress, b.propertyaddress) 
+select a.parcelid,a.propertyaddress, b.parcelid,b.propertyaddress, COALESCE(a.propertyaddress, b.propertyaddress) 
 from housing a
 join housing b
-on a.parcelid = b.parcelid
+on  a.parcelid = b.parcelid
 and a.uniqueid <> b.uniqueid
 and a.propertyaddress is null
 order by a.parcelid 
 
-
+-- b) Update the data
+    
 update housing a
 set propertyaddress = coalesce(a.propertyaddress, b.propertyaddress)
 from housing b
@@ -74,15 +68,13 @@ and a.uniqueid <> b.uniqueid
 and a.propertyaddress is null;
 
 
--- Breaking out address into individual column(address, city,state)
-select propertyaddress
-from housing
-'''
-so, if take a look at it we are gonna realized that in propertyaddress column
+---- Breaking out address into individual column(address, city,state)
+/*
+If take a look at it we are gonna realized that in propertyaddress column
 there is a address and city name separated by a delimeter or a comma
-'''
+*/
 
-/* breaking out propertyaddres column into address & city */
+-- a) Breaking out propertyaddres column into address & city */
 
 alter table housing
 add column property_address varchar(50)
@@ -97,7 +89,7 @@ update housing
 set property_city = split_part(propertyaddress,',',2)
 ---------------------------------------------------------------
 
-/* breaking out owneraddress column into address, city, state */
+-- b) Breaking out owneraddress column into address, city, state 
 
 alter table housing
 add column owner_address varchar(50)
@@ -118,24 +110,24 @@ update housing
 set owner_state = split_part(owneraddress,',',3)
 
 
--- Change Y and N to Yes and No in "Sold as Vacant" field
-
+---- Change Y and N to Yes and No in "Sold as Vacant" field
+    
+-- a) Checking and counting how many types of descriptions are there in the column 'soldasvacant'
+    
 select distinct soldasvacant, count(soldasvacant)
 from housing
 group by 1
 order by 2
 
-
+-- b) Update column
+    
 update housing
 set soldasvacant = case when soldasvacant = 'Y' then 'Yes'
 						when soldasvacant = 'N' then 'No'
 						else soldasvacant
 						end
 ---------------------------------------------------------------
-/* Note : we're not actually gonna remove the data that i wrote below from the actual data, because its kind of risky if we 
-		  do it for real dataset. And also, as we can see in the table that we already set up the uniqueid as the primary key 
-		  from the beginning which is means that there is no duplicate value in out primary key. but just in case if in some moment
-		  we want to  remove the duplicate in a table, then  can to this.
+/* Note : I won't actually execute the query provided below on the real dataset to remove the data. It carries a certain level of risk to do so on actual data.                       Furthermore, as we can observe from the table, we initially established the uniqueid as the primary key, which ensures the absence of duplicate values in this              crucial field. Nevertheless, there might be scenarios where we decide to eliminate duplicates in the table at some point in the future, and if such a need                  arises, we can follow this approach.
 */
 
 -- Remove duplicates using windows function
@@ -155,7 +147,6 @@ delete
 from duplicate_values
 where row_num > 1
 		
-
 -- Delete Unused column 
 /* 
 We delete propertyaddress and owneraddress column bcuz we already update both of this column into readable data that we've done 
@@ -168,4 +159,3 @@ drop column propertyaddress
 
 alter table housing 
 drop column owneraddress
-
